8d2aab9fc0a092bbfde10d2e5d7f66119435bb18
Sara Gorecki <sgorecki@gmail.com>
2014-08-24 19:05:56 -0400

    Create board object and move relevant functions to board prototype. Create start and stop methods using setInterval.

--- js/board.js
+++ js/board.js
@@ -1,11 +1,33 @@
-var width = 10;
-var height = 20;
+var Board = function(width, height){
+  this.grid = this.createBoardGrid(width, height);
+  this.playing = false;
+  this.intervalId;
+};
 
-function createBoardGrid(width, height){
+Board.prototype.createBoardGrid = function(width, height){
   var board = [];
   for (var row = 0; row < height; row++) {
     var innerArray = new Array(width);
     board.push(innerArray);
   }
   return board;
+}
+
+Board.prototype.addPiece = function(){
+  var cell = $('.row').first().children()[4]
+  $(cell).addClass('active');
+}
+
+Board.prototype.start = function(){
+  this.addPiece();
+  this.intervalId = window.setInterval(this.tick.bind(this), 500);
+}
+
+Board.prototype.stop = function(){
+  window.clearInterval(this.intervalId);
+}
+
+Board.prototype.tick = function(){
+  console.log('tick', this);
+
 }
--- js/tetris.js
+++ js/tetris.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
-  var board = createBoardGrid(10, 20);
+  var height = 20;
+  var width = 10;
+  var board = new Board(width, height);
+  window.board = board;
 
-
-  var theData = {boardGrid: board};
+  var theData = {boardGrid: board.grid};
 
   var templateScript = $("#game").html();
   console.log(templateScript);
   var template = Handlebars.compile(templateScript);
   console.log(template(theData));
   $(document.body).append(template(theData));
+
+  $('.start').on('click', function(){
+    board.start();
+  });
 
 });
