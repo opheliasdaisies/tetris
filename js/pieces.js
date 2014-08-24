2d1b60e9f29de228dcdfd1cb9cdeba098d89c533
Sara Gorecki <sgorecki@gmail.com>
2014-08-24 18:04:42 -0400

    Add pieces and different positions for each piece

---
+++ js/pieces.js
@@ -0,0 +1,120 @@
+var pieces = [
+  {
+    color: white,
+    positions: [
+      [
+        [true],
+        [true],
+        [true],
+        [true]
+      ],
+      [
+        [true, true, true, true]
+      ]
+    ]
+  },
+  {
+    color: red,
+    positions: [
+      [
+        [true, true],
+        [true, true]
+      ]
+    ]
+  },
+  {
+    color: yellow,
+    positions: [
+      [
+        [true, true],
+        [, true],
+        [, true]
+      ],
+      [
+        [,, true],
+        [true, true, true]
+      ],
+      [
+        [true],
+        [true],
+        [true, true]
+      ],
+      [
+        [true, true, true],
+        [true]
+      ]
+    ]
+  },
+  {
+    color: purple,
+    positions: [
+      [
+        [true, true],
+        [true],
+        [true]
+      ],
+      [
+        [true, true, true],
+        [,, true]
+      ],
+      [
+        [, true],
+        [, true],
+        [true, true]
+      ],
+      [true],
+      [true, true, true]
+    ]
+  },
+  {
+    color: blue,
+    positions: [
+      [
+        [, true],
+        [true, true, true]
+      ],
+      [
+        [true],
+        [true, true],
+        [true]
+      ],
+      [
+        [true, true, true],
+        [, true]
+      ],
+      [
+        [, true],
+        [true, true],
+        [, true]
+      ]
+    ]
+  },
+  {
+    color: orange,
+    positions: [
+      [
+        [true, true],
+        [, true, true]
+      ],
+      [
+        [, true],
+        [true, true],
+        [true]
+      ]
+    ]
+  },
+  {
+    color: green,
+    positions: [
+      [
+        [, true, true],
+        [true, true]
+      ],
+      [
+        [true],
+        [true, true],
+        [, true]
+      ]
+    ]
+  }
+]
